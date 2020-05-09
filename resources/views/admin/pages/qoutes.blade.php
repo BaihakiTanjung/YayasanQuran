e6aea7b9fc3eb6aecb2621df5093ccfa4a8da21a
tegarhartady <h.tegar21@gmail.com>
2020-05-09 16:44:43 +0700

    Ini update terbarunya

---
+++ resources/views/admin/pages/qoutes.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layouts.default')
+@section('content')
+    <h1>ini halaman qoutes</h1>
+@endsection
---
+++ resources/views/admin/pages/slider.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layouts.default')
+@section('content')
+    <h1>ini halaman slider</h1>
+@endsection
